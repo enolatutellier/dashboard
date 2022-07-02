68435ab051c8a679b9ce24062a39dcec8f6d637a
Enola Tutellier <enola.tutellier@hotmail.com>
2022-07-02 22:05:11 +0200

    version migrate

--- src/Entity/Dashboard.php
+++ src/Entity/Dashboard.php
@@ -13,8 +13,53 @@ class Dashboard
     #[ORM\Column(type: 'integer')]
     private $id;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $marque;
+
+    #[ORM\Column(type: 'integer')]
+    private $taille;
+
+    #[ORM\Column(type: 'integer')]
+    private $prix;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getTaille(): ?int
+    {
+        return $this->taille;
+    }
+
+    public function setTaille(int $taille): self
+    {
+        $this->taille = $taille;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
     }
 }
