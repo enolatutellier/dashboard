1734080f05133c259c394e7cd4a1580cfe014ee0
Enola Tutellier <enola.tutellier@hotmail.com>
2022-07-02 19:54:01 +0200

    création entity(nom bdd)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'Enola',
+            'controller_name' => 'Enola', // vu twig de index.html.twig  controller_name
         ]);
     }
 }
